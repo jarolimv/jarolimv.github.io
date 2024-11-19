de77076d168e708510b68592a80c2c2b16cf7f60
jarolimv <81492980+jarolimv@users.noreply.github.com>
2024-11-19 22:25:25 +0100

    Add files via upload

---
+++ js/links.js
@@ -0,0 +1,14 @@
+export const links = [
+        {
+        name: "BlueSky",
+        link: 
+"https://bsky.app/profile/slavedafy.cz",
+    },
+
+    {
+        name: "Twitter",
+        link: 
+"https://x.com/slave_Dafy",
+    },
+
+];
